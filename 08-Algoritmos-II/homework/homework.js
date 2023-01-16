aa7753790d88702d30a62524386eb6fa6818677a
Leandro Fusetti <leandrofusetti@gmail.com>
2023-01-16 18:04:26 -0300

    terminado

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,26 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length<=1) return array
+  let pivot= array[Math.floor(Math.random()*array.length)]
+  let equals=[]
+  let left=[]
+  let right =[]
 
+  for(let i=0;i<array.length;i++){
+    if(array[i]!==pivot){
+      if(array[i]>pivot){
+        right.push(array[i])  
+      }
+      else{
+        left.push(array[i])
+      }
+    }
+    else{
+      equals.push(array[i])
+    }
+  }
+  return quickSort(left).concat(equals).concat(quickSort(right))
 }
 
 function mergeSort(array) {
@@ -15,6 +34,30 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  //caso base
+  if(array.length<=1)return array
+  let mitad= Math.floor(array.length/2)
+
+  let left= array.slice(0,mitad)
+  let right=array.slice(mitad)
+
+  let newArray= []
+  
+  left =mergeSort(left)
+  right=mergeSort(right)
+
+  while(left.length &&right.length){
+    if(left[0]< right[0]) {
+      newArray.push(left.shift())
+    }
+    else{
+      newArray.push(right.shift())
+    }
+  }
+
+  //concatenar
+  newArray= newArray.concat(left, right)
+  return newArray
   //okey probemos ahora
 }
 
